8c3fef1a354a60bfb69e5acebe22be3f70f2415f
DevendraMRaiyani <17ceuog022@gmail.com>
2020-03-06 21:23:03 +0530

    Summary Reports

--- LPG Distribution System/LPG Distribution System/Accounting/StoveandRegulatorReport.Designer.cs
+++ LPG Distribution System/LPG Distribution System/Accounting/StoveandRegulatorReport.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -56,6 +59,7 @@
             this.txStoveRegulatorBindingSource3 = new System.Windows.Forms.BindingSource(this.components);
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.txStoveRegulatorBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txStoveRegulatorBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txStoveRegulatorBindingSource2)).BeginInit();
@@ -89,7 +93,7 @@
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(655, 34);
+            this.comboBox1.Location = new System.Drawing.Point(606, 34);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(277, 28);
@@ -100,7 +104,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label3.Location = new System.Drawing.Point(587, 37);
+            this.label3.Location = new System.Drawing.Point(538, 37);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 20);
@@ -111,7 +115,7 @@
             // 
             this.dateTimePicker2.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.dateTimePicker2.Location = new System.Drawing.Point(352, 36);
+            this.dateTimePicker2.Location = new System.Drawing.Point(303, 36);
             this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(161, 26);
@@ -121,7 +125,7 @@
             // 
             this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.dateTimePicker1.Location = new System.Drawing.Point(116, 34);
+            this.dateTimePicker1.Location = new System.Drawing.Point(67, 34);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(161, 26);
@@ -131,7 +135,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(325, 36);
+            this.label2.Location = new System.Drawing.Point(276, 36);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 20);
@@ -142,7 +146,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(73, 36);
+            this.label1.Location = new System.Drawing.Point(24, 36);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 20);
@@ -156,7 +160,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button1.Location = new System.Drawing.Point(1233, 32);
+            this.button1.Location = new System.Drawing.Point(1154, 32);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 30);
             this.button1.TabIndex = 60;
@@ -179,6 +183,14 @@
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.txIdDataGridViewTextBoxColumn,
@@ -193,9 +205,25 @@
             this.sGSTDataGridViewTextBoxColumn,
             this.totalDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.txStoveRegulatorBindingSource3;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(77, 122);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
@@ -312,7 +340,7 @@
             this.checkBox2.Checked = true;
             this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkBox2.Location = new System.Drawing.Point(1076, 36);
+            this.checkBox2.Location = new System.Drawing.Point(1027, 36);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(102, 24);
             this.checkBox2.TabIndex = 63;
@@ -325,19 +353,31 @@
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkBox1.Location = new System.Drawing.Point(991, 36);
+            this.checkBox1.Location = new System.Drawing.Point(942, 36);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(59, 24);
             this.checkBox1.TabIndex = 62;
             this.checkBox1.Text = "Sell";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(1345, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 30);
+            this.button2.TabIndex = 64;
+            this.button2.Text = "Export as Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // StoveandRegulatorReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(982, 753);
+            this.ClientSize = new System.Drawing.Size(1567, 753);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.dataGridView1);
@@ -400,5 +440,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn totalDataGridViewTextBoxColumn;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- LPG Distribution System/LPG Distribution System/Accounting/SummaryReport.cs
+++ LPG Distribution System/LPG Distribution System/Accounting/SummaryReport.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
 
 namespace LPG_Distribution_System.Accounting
 {
@@ -19,12 +20,12 @@ namespace LPG_Distribution_System.Accounting
 
         private void SummaryReport_Load(object sender, EventArgs e)
         {
-            Location = new Point(-7, 50);
+            Location = new System.Drawing.Point(-7, 50);
             int w = SystemInformation.VirtualScreen.Width + 14;
             int h = SystemInformation.VirtualScreen.Height - 43;
             Size = new Size(w, h);
-            dataGridView1.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 10);
-            dataGridView2.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 10);
+            dataGridView1.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
+            dataGridView2.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
 
         }
 
@@ -32,7 +33,7 @@ namespace LPG_Distribution_System.Accounting
         {
             DateTime from = dateTimePicker1.Value;
             DateTime to = dateTimePicker2.Value;
-            
+
             using (AccountingRef.AccountingClient client = new AccountingRef.AccountingClient())
             {
                 var result = client.GetTxCylinders(from, to, "");
@@ -40,15 +41,182 @@ namespace LPG_Distribution_System.Accounting
                 dataGridView1.DataSource = groups.ToList();
                 groups = result.Where(x => x.CashMemoNo != 0).AsEnumerable().GroupBy(g => g.CylinderDetails).Select(g => new { CylinderDetails = g.Key, Price = Math.Round(g.Sum(s => s.Price) / g.Count(), 2), Quentity = g.Sum(s => s.Quentity), Total = g.Sum(s => s.Total) });
                 dataGridView3.DataSource = groups.ToList();
-                
+
                 var result1 = client.GetTxStoveRegulators(from, to, "");
                 var groups1 = result1.Where(x => x.CashMemoNo == 0).AsEnumerable().GroupBy(g => g.Details).Select(g => new { Details = g.Key, Price = Math.Round(g.Sum(s => s.Price) / g.Count(), 2), Quentity = g.Sum(s => s.Quentity), Total = g.Sum(s => s.Total) });
                 dataGridView2.DataSource = groups1.ToList();
-                groups1 = result1.Where(x => x.CashMemoNo != 0).AsEnumerable().GroupBy(g => g.Details).Select(g => new { Details = g.Key, Price = Math.Round(g.Sum(s=>s.Price)/g.Count(),2),Quentity = g.Sum(s=>s.Quentity), Total = g.Sum(s=>s.Total) });
+                groups1 = result1.Where(x => x.CashMemoNo != 0).AsEnumerable().GroupBy(g => g.Details).Select(g => new { Details = g.Key, Price = Math.Round(g.Sum(s => s.Price) / g.Count(), 2), Quentity = g.Sum(s => s.Quentity), Total = g.Sum(s => s.Total) });
                 dataGridView4.DataSource = groups1.ToList();
-               
+
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            /*Microsoft.Office.Interop.Excel.Application xcelApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook wb = xcelApp.Application.Workbooks.Add(Type.Missing);
+            //Microsoft.Office.Interop.Excel.Worksheet ws1 = wb.ActiveSheet;
+            //ws1.Name = "WS1";
+
+            Microsoft.Office.Interop.Excel.Sheets worksheets = wb.Worksheets;
+
+            var xlNewSheet = (Microsoft.Office.Interop.Excel.Worksheet)worksheets.Add(worksheets[1], Type.Missing, Type.Missing, Type.Missing);
+            xlNewSheet.Name = "new1sheet";
+
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                xlNewSheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    xlNewSheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            xlNewSheet.Columns.AutoFit();
+
+            Microsoft.Office.Interop.Excel.Worksheet ws2 = wb.ActiveSheet;
+            ws2.Name = "WS2";
+            for (int i = 1; i < dataGridView2.Columns.Count + 1; i++)
+            {
+                ws2.Cells[1, i] = dataGridView2.Columns[i - 1].HeaderText;
+            }
+
+            for (int i = 0; i < dataGridView2.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView2.Columns.Count; j++)
+                {
+                    ws2.Cells[i + 2, j + 1] = dataGridView2.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+
+            ws2.Columns.AutoFit();*/
+
+            // creating Excel Application
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            // creating new WorkBook within Excel application
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            // creating new Excelsheet in workbook
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+
+            Sheets xlSheets = null;
+            Worksheet xlNewSheet = null;
+
+
+            xlSheets = workbook.Sheets as Sheets;
+
+            if (dataGridView1.Rows.Count > 0)
+            {
+                //Select the sheet
+                worksheet = workbook.Worksheets[1];
+                //Rename the sheet
+                worksheet.Name = "Cylinder Purchased";
+                for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+                {
+                    worksheet.Cells[1, i].EntireRow.Font.Bold = true;
+                    worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+                }
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+                worksheet.Columns.AutoFit();
+            }
+
+            if (dataGridView2.Rows.Count > 0)
+            {
+                // The first argument below inserts the new worksheet as the first one
+                xlNewSheet = (Worksheet)xlSheets.Add(xlSheets[1], Type.Missing, Type.Missing, Type.Missing);
+                xlNewSheet.Name = "Stove&Regulator Purchased";
+                for (int i = 1; i < dataGridView2.Columns.Count + 1; i++)
+                {
+                    xlNewSheet.Cells[1, i].EntireRow.Font.Bold = true;
+                    xlNewSheet.Cells[1, i] = dataGridView2.Columns[i - 1].HeaderText;
+                }
+
+                for (int i = 0; i < dataGridView2.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView2.Columns.Count; j++)
+                    {
+                        xlNewSheet.Cells[i + 2, j + 1] = dataGridView2.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+
+                xlNewSheet.Columns.AutoFit();
+            }
+
+            if (dataGridView3.Rows.Count > 0)
+            {
+                // The first argument below inserts the new worksheet as the first one
+                xlNewSheet = (Worksheet)xlSheets.Add(xlSheets[1], Type.Missing, Type.Missing, Type.Missing);
+                xlNewSheet.Name = "Cylinder Sold";
+                for (int i = 1; i < dataGridView3.Columns.Count + 1; i++)
+                {
+                    xlNewSheet.Cells[1, i].EntireRow.Font.Bold = true;
+                    xlNewSheet.Cells[1, i] = dataGridView3.Columns[i - 1].HeaderText;
+                }
+
+                for (int i = 0; i < dataGridView3.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView3.Columns.Count; j++)
+                    {
+                        xlNewSheet.Cells[i + 2, j + 1] = dataGridView3.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+
+                xlNewSheet.Columns.AutoFit();
+            }
+
+            if (dataGridView4.Rows.Count > 0)
+            {
+                // The first argument below inserts the new worksheet as the first one
+                xlNewSheet = (Worksheet)xlSheets.Add(xlSheets[1], Type.Missing, Type.Missing, Type.Missing);
+                xlNewSheet.Name = "Stove&Regulator Sold";
+                for (int i = 1; i < dataGridView4.Columns.Count + 1; i++)
+                {
+                    xlNewSheet.Cells[1, i].EntireRow.Font.Bold = true;
+                    xlNewSheet.Cells[1, i] = dataGridView4.Columns[i - 1].HeaderText;
+                }
+
+                for (int i = 0; i < dataGridView4.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView4.Columns.Count; j++)
+                    {
+                        xlNewSheet.Cells[i + 2, j + 1] = dataGridView4.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+
+                xlNewSheet.Columns.AutoFit();
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = @"C:\";
+            sfd.RestoreDirectory = true;
+            sfd.FileName = "*.xlsx";
+            sfd.DefaultExt = "xlsx";
+            sfd.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string path = sfd.FileName;
+                workbook.SaveAs(path, Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook,
+                                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                                Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                workbook.Close(Type.Missing, Type.Missing, Type.Missing);
+                app.UserControl = true;
+                app.Quit();
+                MessageBox.Show("Report is exported successfully!!!");
+            }
+
+        }
+ 
 
         /*protected override void WndProc(ref Message message)
         {
